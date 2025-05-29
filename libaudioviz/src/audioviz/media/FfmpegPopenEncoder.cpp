3c954ea847ec186f57b39eeed27eaf60663e1ff6
ChickenPige0n <2336983354@qq.com>
2025-05-29 15:33:37 +0800

    use pbo to optimize speed....

--- libaudioviz/src/audioviz/media/FfmpegPopenEncoder.cpp
+++ libaudioviz/src/audioviz/media/FfmpegPopenEncoder.cpp
@@ -1,3 +1,5 @@
+#include "_deps/sfml-src/extlibs/headers/glad/include/glad/gl.h"
+#include "_deps/sfml-src/include/SFML/Window/GlResource.hpp"
 #include <audioviz/media/FfmpegPopenEncoder.hpp>
 #include <audioviz/util.hpp>
 #include <cstring>
@@ -15,6 +17,23 @@ namespace audioviz
 FfmpegPopenEncoder::FfmpegPopenEncoder(
 	const audioviz::Base &viz, const std::string &outfile, const std::string &vcodec, const std::string &acodec)
 {
+	// pbos = {0};
+	video_size = viz.size;
+	glGenBuffers(NUM_PBOS, pbos);
+
+	const unsigned int byte_size = viz.size.x * viz.size.y * 4; // 4 refers to channel count
+	for (unsigned int pbo : pbos) {
+		glBindBuffer(GL_PIXEL_PACK_BUFFER, pbo);
+		glBufferData(GL_PIXEL_PACK_BUFFER, byte_size, nullptr, GL_STREAM_READ);
+	}
+	glBindBuffer(GL_PIXEL_PACK_BUFFER, 0);
+
+
+
+	glGenFramebuffers(1, &fbo);
+	glBindFramebuffer(GL_FRAMEBUFFER, fbo);
+
+
 	const auto &url = viz.get_media_url();
 	std::ostringstream cmd_stream;
 	cmd_stream << "ffmpeg -hide_banner -hwaccel auto -y ";
@@ -66,6 +85,34 @@ FfmpegPopenEncoder::~FfmpegPopenEncoder()
 		ffmpeg = nullptr;
 	}
 }
+void FfmpegPopenEncoder::send_frame(const sf::Texture &txr)
+{
+	glBindTexture(GL_TEXTURE_2D, txr.getNativeHandle());
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, video_size.x, video_size.y, 0, GL_RGBA, GL_UNSIGNED_BYTE, nullptr);
+
+	glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, txr.getNativeHandle(), 0);
+
+	glBindBuffer(GL_PIXEL_PACK_BUFFER, pbos[current_frame % NUM_PBOS]);
+	glReadPixels(0, 0, video_size.x, video_size.y, GL_RGBA, GL_UNSIGNED_BYTE, 0);
+
+	// Map previous PBO and write to FFmpeg
+	int prev_idx = (current_frame + 1) % NUM_PBOS;
+	if (current_frame >= NUM_PBOS - 1) { // Only start reading after we've filled the queue
+		glBindBuffer(GL_PIXEL_PACK_BUFFER, pbos[prev_idx]);
+		auto ptr = static_cast<std::uint8_t*>(glMapBuffer(GL_PIXEL_PACK_BUFFER, GL_READ_ONLY));
+		if (ptr) {
+			fwrite(ptr, 1, video_size.x * video_size.y * 4, ffmpeg);
+		}
+		glUnmapBuffer(GL_PIXEL_PACK_BUFFER);
+	}
+	glBindBuffer(GL_PIXEL_PACK_BUFFER, 0);
+	glBindFramebuffer(GL_FRAMEBUFFER, 0);
+	glBindTexture(GL_TEXTURE_2D, 0);
+	glFlush();
+	current_frame++;
+
+	// send_frame(txr.copyToImage());
+}
 
 void FfmpegPopenEncoder::send_frame(const sf::Image &img)
 {
@@ -74,5 +121,6 @@ void FfmpegPopenEncoder::send_frame(const sf::Image &img)
 	if (fwrite(pixels, 1, size, ffmpeg) != size)
 		throw std::runtime_error("Failed to write frame to ffmpeg stdin");
 }
+
 
 } // namespace audioviz::media
