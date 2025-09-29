b63894c50d6029093c29c682bc9b40775d321f58
trustytrojan <87675609+trustytrojan@users.noreply.github.com>
2025-09-29 15:39:21 -0400

    cmake/ci: cleanup code such that rebuilds arent necessary on every reconfigure (#24)
    remove ffmpeg finding because we dont need it to build
    
    run ci only on c++ and cmake files changed

--- ttviz/deps.cmake
+++ ttviz/deps.cmake
@@ -1,10 +1,12 @@
 include(FetchContent)
 
 # argparse
-file(DOWNLOAD https://github.com/p-ranav/argparse/raw/master/include/argparse/argparse.hpp ${CMAKE_BINARY_DIR}/argparse.hpp)
+if(NOT EXISTS ${CMAKE_BINARY_DIR}/argparse.hpp)
+	file(DOWNLOAD https://github.com/p-ranav/argparse/raw/master/include/argparse/argparse.hpp ${CMAKE_BINARY_DIR}/argparse.hpp)
+endif()
 
 # imgui
-FetchContent_Declare(imgui URL https://github.com/ocornut/imgui/archive/v1.91.8.zip)
+FetchContent_Declare(imgui URL https://github.com/ocornut/imgui/archive/v1.91.8.tar.gz)
 FetchContent_MakeAvailable(imgui)
 
 # imgui-sfml (depends on imgui being downloaded manually)
@@ -18,5 +20,5 @@ else()
 	# based on BUILD_SHARED_LIBS
 endif()
 set(IMGUI_DIR ${imgui_SOURCE_DIR})
-FetchContent_Declare(imgui-sfml URL https://github.com/SFML/imgui-sfml/archive/v3.0.zip)
+FetchContent_Declare(imgui-sfml URL https://github.com/SFML/imgui-sfml/archive/v3.0.tar.gz)
 FetchContent_MakeAvailable(imgui-sfml)
